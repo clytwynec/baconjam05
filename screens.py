665a200705d1dc589fb0e09076e0c7755b9346f4
Stephen Eckenrode <stephen.eckenrode@gmail.com>
2013-06-08 14:33:37 -0400

    Added frame for GameMain screen

--- screens.py
+++ screens.py
@@ -1,3 +1,8 @@
+import os
+import sys
+import random
+import pygame
+
 import engine
 
 ###################################################
@@ -9,6 +14,55 @@ class TestScreen(engine.Screen):
         engine.Screen.initialize(self)
 
         print "This is a test of the screen system"
+
+###################################################
+
+class GameMain(engine.Screen):
+    def __init__(self, kernel):
+        engine.Screen.__init__(self, kernel, 'GameMain')
+
+        self.last_mouse_x = pygame.mouse.get_pos()[0]
+        self.shakes = 0
+        self.ticks = 0
+        self.mouse_moving_right = False
+
+        self.current_garmet = None
+
+    def initialize(self):
+        engine.Screen.initialize(self)
+
+    def handle_event(self, event):
+        if event.type == pygame.MOUSEMOTION:
+            current_mouse_x = pygame.mouse.get_pos()[0]
+
+            # Calculate shakes.  This is done by counting how many times
+            # the mouse has changed direction within a given timeframe
+            if self.mouse_moving_right and self.last_mouse_x > self.current_mouse_x:
+                self.shakes += 1
+                self.mouse_moving_right = False
+            elif self.mouse_moving_right and self.last_mouse_x < self.current_mouse_x:
+                self.shakes += 1
+                self.mouse_moving_right = True
+
+            self.last_mouse_x = current_mouse_x
+
+            # If we've changed directions more than 5 times, then
+            # trigger the shake event for the current garmet and
+            # reset the counter
+            if self.shakes >= 5:
+                self.shakes = 0
+
+                if self.current_garmet:
+                    self.current_garmet.shake()
+
+
+    def update(self, delta):
+        self.ticks += delta
+
+        if (self.ticks >= 200):
+            self.ticks = 0
+            self.shakes = max(self.shakes - 1, 0)
+
 
 ###################################################
 
@@ -54,4 +108,3 @@ class MenuBase(engine.Screen):
             self.kernel.display_surface.blit(self.menu_items[item], self.menu_rects[item])
 
         return engine.screen.update(self, delta)
-        
