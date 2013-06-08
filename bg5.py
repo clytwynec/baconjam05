ed30ef84d3ad2371c26facf57de8a4adb019af77
Stephen Eckenrode <stephen.eckenrode@gmail.com>
2013-06-08 14:32:52 -0400

    Added FPS Rectangle

--- bg5.py
+++ bg5.py
@@ -3,16 +3,32 @@
 import engine
 import screens
 
+import pygame
+
 kernel = engine.Kernel()
 
-kernel.initialize_display((800, 600))
+screen_surf = kernel.initialize_display((800, 600))
 
 kernel.screen_manager.register_screen(engine.Screen(kernel, 'Testing'))
 kernel.screen_manager.register_screen(screens.TestScreen(kernel))
 
 kernel.screen_manager.switch_to('Test Screen')
 
+font = pygame.font.SysFont("Helvetica", 12)
+
 while 1:
-    kernel.process_system_event()
+    delta = kernel.ticker.get_time()
+    
+    kernel.process_events()
+
+    kernel.screen_manager.update(delta)
+
+    # Render the FPS display
+    fps_surf = font.render("FPS: " + str(int(kernel.ticker.get_fps())), True, (255, 255, 255))
+    fps_rect = fps_surf.get_rect()
+    fps_rect.topright = screen_surf.get_rect().topright
+    screen_surf.blit(fps_surf, fps_rect)
 
     kernel.flip_display()
+
+    kernel.ticker.tick(60)
